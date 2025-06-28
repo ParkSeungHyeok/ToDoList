f276c119c5f0b9ed59da54630f4d22b1bb81ed5b
ParkSeungHyeok <tmdgur1324@naver.com>
2025-06-29 00:34:38 +0900

    use StatelessWidget and StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('To Do List 실험 앱')),
-        body: Center(child: Text('여기에 To Do가 들어갈 예정!')),
-      ),
-    ),
-  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: const MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('To Do List 실험 앱')),
+      body: Center(child: Text('여기에 To Do가 들어갈 예정!')),
+    );
+  }
 }
