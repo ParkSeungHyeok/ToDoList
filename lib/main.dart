dfba30971e015e64594e09b5d8ed189577f6824c
ParkSeungHyeok <tmdgur1324@naver.com>
2025-06-30 22:26:18 +0900

    add button and additem feature

--- lib/main.dart
+++ lib/main.dart
@@ -16,27 +16,14 @@ class ToDoScreen extends StatefulWidget {
   const ToDoScreen({super.key});
 
   @override
-  State<ToDoScreen> createState() => _ToDoScreenState();
+  State<ToDoScreen> createState() {
+    return _ToDoScreenState();
+  }
 }
 
 class _ToDoScreenState extends State<ToDoScreen> {
   TextEditingController inputTextController = TextEditingController();
-
-  Widget inputToDoItem() {
-    return TextField(controller: inputTextController);
-  }
-
-  Widget outputToDoList() {
-    return Expanded(
-      child: ListView(
-        children: [
-          Text("여기에 To Do가 들어갈 예정 "),
-          Text("여기에 To Do가 들어갈 예정 2"),
-          Text("여기에 To Do가 들어갈 예정 3"),
-        ],
-      ),
-    );
-  }
+  List<String> toDoList = [];
 
   @override
   Widget build(BuildContext context) {
@@ -44,5 +31,36 @@ class _ToDoScreenState extends State<ToDoScreen> {
       appBar: AppBar(title: Text('To Do List')),
       body: Column(children: [inputToDoItem(), outputToDoList()]),
     );
+  }
+
+  Widget inputToDoItem() {
+    return Row(
+      children: [
+        Expanded(child: TextField(controller: inputTextController)),
+        ElevatedButton(onPressed: _addTodo, child: Text('추가')),
+      ],
+    );
+  }
+
+  void _addTodo() {
+    if (inputTextController.text.isEmpty) return;
+
+    setState(() {
+      toDoList.add(inputTextController.text);
+    });
+
+    inputTextController.clear();
+  }
+
+  Widget outputToDoList() {
+    return Expanded(child: ListView(children: gettoDoListWidget()));
+  }
+
+  List<Widget> gettoDoListWidget() {
+    List<Widget> toDoListWidget = [];
+    for (int i = 0; i < toDoList.length; i++) {
+      toDoListWidget.add(Text(toDoList[i]));
+    }
+    return toDoListWidget;
   }
 }
